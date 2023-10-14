1b2ebd20a9bb62497683754e93615b5fee56bb94
akshay kotkar <akshaynkotkar@gmail.com>
2023-10-14 06:35:56 +0530

    adding binary to decimal code

---
+++ src/alpha/prime_nos.java
@@ -0,0 +1,39 @@
+package alpha;
+
+import java.util.Scanner;
+
+public class prime_nos {
+    static boolean isPrime(int n)
+    {
+        if(n==2)
+        {
+            return true;
+        }
+
+        for(int i=2;i<Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void primesInRange(int n)
+    {
+        for(int i=2;i<=n;i++)
+        {
+            if(isPrime(i))
+            {
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n=sc.nextInt();
+        primesInRange(n);
+    }
+}
