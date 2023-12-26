9ba4a1d73a70c53c42a7d185f50249ffaf89849f
akshay kotkar <akshaynkotkar@gmail.com>
2023-12-26 18:16:14 +0530

    reverse a string recursion modified

--- src/recursion/String/reverseString.java
+++ src/recursion/String/reverseString.java
@@ -17,10 +17,20 @@ public class reverseString {
         char currChar=s.charAt(0);
         return revrese+currChar;
     }
+    public  static String reverse2(String s,int idx)
+    {
+        if(idx==s.length()) return "";
+        String revrese =reverse2(s,idx+1);
+        char currChar=s.charAt(idx);
+        return revrese+currChar;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a String:");
         String s=sc.nextLine();
+        System.out.println("Reverse string ");
         System.out.println(reverse(s));
+
+        System.out.println(reverse2(s,0));
     }
 }
