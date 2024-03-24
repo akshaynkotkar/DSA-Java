fb37ddb7b405fafdbc11c4ea6992362b20dece81
akshay kotkar <akshaynkotkar@gmail.com>
2024-03-24 18:58:36 +0530

    fib using DP with tabulation and memorization

--- src/DP/fibonacci.java
+++ src/DP/fibonacci.java
@@ -2,6 +2,7 @@
 package DP;
 import java.util.Scanner;
 public class fibonacci {
+    //Memorization
     public static  int fib(int n, int dp[])
     {
         if(n==0 || n==1)
@@ -15,6 +16,18 @@ public class fibonacci {
         dp[n]=fib(n-1,dp)+fib(n-2,dp);
         return dp[n];
     }
+    //Tabulation
+    public static int fibTabulation(int n)
+    {
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the value of n:");
@@ -22,5 +35,8 @@ public class fibonacci {
         int dp[]=new int[n+1];
 
         System.out.println("Nth fibonacci number is:"+fib(n,dp));
+        System.out.println();
+        System.out.println("Nth fibonacci using tabulation is:"+fibTabulation(n));
+
     }
 }
